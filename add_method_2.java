db609774d0b584587a101738b05ec7d6e8cb7acd
Revati Kamble <revati2855@gmail.com>
2026-08-07 06:37:25 -0700

    Method with return value (sum)

---
+++ add_method_2.java
@@ -0,0 +1,10 @@
+public class add_method_2 {
+    static int add(int a, int b) {
+        return a+b ;
+    }
+    public static void main(String[] args) {
+        int result = add(22,90);
+        System.out.println(result);
+    }
+    
+}
